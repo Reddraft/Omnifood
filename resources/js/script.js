034fa9ef6a168c9a285a1cd5057d3fbada621725
Ricardo <rick@reddraft.com>
2016-01-06 22:15:25 -0500

    Made Responsive navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -92,9 +92,25 @@ $(document).ready(function() {
   });       
 
     
-    
+  /* --------- MOBILE NAVIGATION ---------*/    
 
+  $('.js--nav-icon').click(function(){
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
 
+    nav.slideToggle(200);
+
+     //Change burger icon to close icon
+     if(icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+     } else {
+        icon.removeClass('ion-close-round');
+        icon.addClass('ion-navicon-round');
+
+     } 
+
+  })
   
 });
 
