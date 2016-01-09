b34df93ac32333f66f7310e245ef51bf7f90f07a
Ricardo <rick@reddraft.com>
2016-01-09 02:38:09 -0500

    Modified Procfile

--- server.js
+++ server.js
@@ -2,16 +2,7 @@ var express = require('express');
 var app = express();
 
 
-//Express 3
-//app.configure(function() {
-//    app.use(express.static(__dirname, '/'));
-//});
-
-//Express 4
-app.use(express.static( __dirname + '/out/setup'));
-app.use(express.static( __dirname + '/out/webapp'));
-
-
+app.use(express.static( __dirname + '/'));
 
 
 app.listen(5000);
